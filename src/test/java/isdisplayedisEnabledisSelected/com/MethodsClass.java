ee18f7d6736a6de71eee9187a949d5d666a9a356
hvpswtester <hvpswtester@gmail.com>
2023-12-24 11:07:47 +0530

    iframe isSelected isEnabled isDisplay

--- src/test/java/isdisplayedisEnabledisSelected/com/MethodsClass.java
+++ src/test/java/isdisplayedisEnabledisSelected/com/MethodsClass.java
@@ -1,11 +1,10 @@
-package JSClick.BrowserCustom.navigate;
+package isdisplayedisEnabledisSelected.com;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,9 +13,8 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class JSClickCustomBrowserSettingNavigate {
-
-public static WebDriver driver;
+public class MethodsClass {
+	public static WebDriver driver;
 	
 	public static void waitForSeconds(int timeOut) {
 		try {
@@ -59,70 +57,31 @@ public static WebDriver driver;
 	      
 	      return driver;
 	  }
-	
-	
-	public static void JSClick(WebDriver driver, WebElement ele, int timeout) {
-		JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
-		jsExecutor.executeScript("arguments[0].setAttribute('style','border:4px solid green; backgroung:red')", ele);
-		jsExecutor.executeScript("arguments[0].click()", ele);
-		waitForSeconds(timeout);
-	}
-	
 	public static void main(String[] args) {
 		driver = getChromeDriver();
 		driver.get("https://www.facebook.com/");
-		System.out.println("Open faceboook.com");
-		
-		WebElement loginBtnElement = driver.findElement(By.name("login"));
-		loginBtnElement.click();
-		System.out.println("click login");
 		waitForSeconds(3);
+		WebElement createNewAccountElement = driver.findElement(By.xpath("//a[text()='Create new account']"));
 		
-		driver.navigate().back();
-		System.out.println("click back, Browser button");
-		waitForSeconds(2);
-		
-		loginBtnElement = driver.findElement(By.name("login"));
-	    JSClick(driver, loginBtnElement, 2);	
-	    System.out.println("Click login");
-	    
-	    waitForSeconds(2);
-	    driver.navigate().to("https://www.google.com/");
-	    System.out.println("Open google.com");
-	    waitForSeconds(1);
-	    driver.navigate().refresh();
-	    waitForSeconds(1);
-	    driver.navigate().back();
-	    waitForSeconds(1);
-	    driver.navigate().forward();
-		
-		waitForSeconds(3);
-		
-		driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_a_download");
-		waitForSeconds(8);
-		
-		driver.switchTo().frame(1);
-		WebElement imgLogoElement = driver.findElement(By.xpath("//img[@alt='W3Schools']"));
-		imgLogoElement.click();
-		waitForSeconds(15);
-		driver.switchTo().defaultContent();
-		
-		WebElement websitElement = driver.findElement(By.xpath("//a[@id='getwebsitebtn']"));
-		websitElement.click();
-		waitForSeconds(5);
-		
-		driver.switchTo().frame("iframeResult");
-		imgLogoElement.click();
-		waitForSeconds(15);
-		driver.switchTo().defaultContent();
-		
-		WebElement iframElement = driver.findElement(By.xpath("//iframe[@name='iframeResult']"));
-		
-		driver.switchTo().frame(iframElement);
-		imgLogoElement.click();
-		waitForSeconds(15);
-		driver.switchTo().defaultContent();
-		
-		driver.quit();
+        System.out.println(createNewAccountElement.isDisplayed());
+        createNewAccountElement.click();
+        waitForSeconds(3);
+        
+        WebElement genderElement = driver.findElement(By.xpath("//label[text()='Female']//..//input"));
+        
+        System.out.println(genderElement.isSelected());
+        System.out.println(genderElement.isEnabled());
+        
+        System.out.println(genderElement.getAttribute("checked"));
+        
+        genderElement.click();
+        
+        System.out.println(genderElement.isSelected());
+        System.out.println(genderElement.isEnabled());
+        System.out.println(genderElement.getAttribute("checked"));
+        waitForSeconds(1);
+        
+        driver.quit();
 	}
+
 }
